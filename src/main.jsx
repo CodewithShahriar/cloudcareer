f8bac626e3e9db4402a427a61a1c0b2e1935fb5b
Abid Shahriar <abid.shahriar.23@gmail.com>
2023-05-24 03:53:03 +0600

    header added

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
-import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import ErrorPage from './components/ErrorPage'
+import Statistics from './components/Statistics'
+import Blogs from './components/Blogs'
+import AppliedJobs from './components/AppliedJobs'
+import StartApplying from './components/StartApplying'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/statistics',
+        element: <Statistics />
+      },
+      {
+        path: '/blogs',
+        element: <Blogs />
+      },
+      {
+        path: '/appliedJobs',
+        element: <AppliedJobs />
+      },
+      {
+        path: '/startApplying',
+        element: <StartApplying />
+      },
+    ]
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} > </RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
